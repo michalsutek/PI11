c3e65125f99e48e5defaca64d25b206b26678055
Michal Šutek <sutek.spsit@gmail.com>
2024-01-24 11:04:56 +0100

    Add 15-funkcie

---
+++ 15-funkcie/kruhy.py
@@ -0,0 +1,25 @@
+import tkinter
+
+canvas = tkinter.Canvas()
+canvas.pack()
+
+
+def kruh(x, y, r, farba):
+    canvas.create_oval(x - r, y - r, x + r, y + r, fill=farba)
+
+
+def kruhy_riadok(x, y, pocet, r, farba):
+    for i in range(pocet):
+        kruh(x, y, r, farba)
+        x = x + r * 2
+
+
+def kruhy_stvorec(x, y, pocet, r, farba):
+    for i in range(pocet):
+        kruhy_riadok(x, y, pocet, r, farba)
+        y = y + r * 2
+
+
+kruhy_stvorec(50, 50, 10, 10, "red")
+
+canvas.mainloop()
